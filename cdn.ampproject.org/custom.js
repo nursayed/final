d241884325171c13b2384e611509df581c160c02
nur sayed <freelancernursayed@gmail.com>
2020-12-23 10:53:49 +0600

    commit

---
+++ cdn.ampproject.org/custom.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName("html")[0].style.display="none";
+
+window.onload=function(){
+
+    //do your stuff
+
+    document.getElementsByTagName("html")[0].style.display="block"; //to show it all back again
+}
